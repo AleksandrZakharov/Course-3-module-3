41a54cdf5944b57d7ef73fa776d98eda77cce857
human.of.rain <hauman.of.rain@gmail.com>
2016-05-17 18:06:47 +0300

    rspec spec/custom_types_spec.rb -e rq02

---
+++ app/models/address.rb
@@ -0,0 +1,59 @@
+class Address
+
+	attr_accessor :city, :state, :location
+
+	def initialize (city,state,location)
+
+		@city = city; @state = state; @location = Point.new(location[:coordinates][0],location[:coordinates][1])
+
+	end
+
+	def mongoize
+
+		{:city=>@city, :state=>@state, :loc=>location.mongoize}
+
+	end
+
+	def self.mongoize object
+
+		case object
+
+			when nil then nil
+
+			when Hash then object
+
+			when Address then object.mongoize
+
+		end
+
+	end
+
+	def self.demongoize object
+
+		case object
+
+			when nil then nil
+
+			when Hash then Address.new(object[:city], object[:state],object[:loc])
+
+			when Address then object
+
+		end
+
+	end
+
+	def self.evolve(object)
+
+	    case object
+
+			when nil then nil
+
+			when Hash then object
+
+			when Address then object.mongoize
+
+		end
+
+  	end
+
+end
---
+++ app/models/point.rb
@@ -0,0 +1,70 @@
+class Point
+
+	attr_accessor :longitude, :latitude
+
+	def initialize (lng, lat)
+
+		@longitude = lng; @latitude = lat
+
+	end
+
+	def mongoize
+
+		{:type=>'Point', :coordinates=>[@longitude,@latitude]}
+
+	end
+
+	def self.mongoize object
+
+		case object
+
+			when nil then nil
+
+			when Hash then 
+
+				if object[:type]
+		          Point.new(object[:coordinates][0], object[:coordinates][1]).mongoize
+		      	else
+		          Point.new(object[:lng], object[:lat]).mongoize
+		      	end
+
+			when Point then object.mongoize
+
+		end
+
+	end
+
+	def self.demongoize object
+
+		case object
+
+			when nil then nil
+
+			when Hash then Point.new(object[:coordinates][0], object[:coordinates][1])
+
+			when Point then object
+
+		end
+
+	end
+
+	def self.evolve(object)
+
+	    case object
+
+			when nil then nil
+
+			when Hash then 
+
+				if object[:type]
+		          Point.new(object[:coordinates][0], object[:coordinates][1]).mongoize
+		      	else
+		          Point.new(object[:lng], object[:lat]).mongoize
+		      	end
+
+			when Point then object.mongoize
+
+		end
+  end
+
+end
