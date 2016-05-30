2ef9007f6c63a4ec7c86750e700ba92eebe7dfac
human.of.rain <hauman.of.rain@gmail.com>
2016-05-30 18:32:00 +0300

    rspec spec/service_facade_spec.rb -e rq03

--- app/models/race.rb
+++ app/models/race.rb
@@ -62,6 +62,48 @@ class Race
 
 	end
 
+	def next_bib
+
+		self.inc(next_bib: 1)
+		return self[:next_bib]
+
+	end
+
+	def get_group racer
+
+		if racer && racer.birth_year && racer.gender
+
+			quotient=(date.year-racer.birth_year)/10
+
+			min_age=quotient*10
+
+			max_age=((quotient+1)*10)-1
+
+			gender=racer.gender
+
+			name=min_age >= 60 ? "masters #{gender}" : "#{min_age} to #{max_age} (#{gender})"
+
+			Placing.demongoize(:name=>name)
+
+		end
+
+	end
+
+	def create_entrant racer
+
+		entrant = Entrant.new
+		entrant.race = self.attributes.symbolize_keys.slice(:_id, :n, :date)
+		entrant.racer = racer.info.attributes
+		entrant.group = get_group (racer)
+		self.events.each { |event| entrant.send("#{event.name}=", event) }
+		if entrant.validate
+			entrant.bib = self.next_bib
+			entrant.save
+		end
+		entrant
+
+	end
+
 	DEFAULT_EVENTS.keys.each do |name|
 
 		define_method("#{name}") do
